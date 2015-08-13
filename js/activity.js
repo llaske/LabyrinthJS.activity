1e9db95b54ec392e00d2f50d9ec4d3932eacd672
Lionel Laské <llaske@c2s.fr>
2015-08-13 22:36:12 +0200

    Handle mode switch in toolbar

--- js/activity.js
+++ js/activity.js
@@ -6,7 +6,25 @@ define(function (require) {
 
         // Initialize the activity.
         activity.setup();
-
+	
+		// Handle toolbar mode switch
+		var currentMode = 0;
+		var nodetextButton = document.getElementById("nodetext-button");
+		var linkButton = document.getElementById("link-button");
+		var removeButton = document.getElementById("delete-button");
+		var switchMode = function(newMode) {
+			currentMode = newMode;
+			nodetextButton.classList.remove('active');			
+			linkButton.classList.remove('active');			
+			removeButton.classList.remove('active');
+			if (newMode == 0) nodetextButton.classList.add('active');
+			else if (newMode == 1) linkButton.classList.add('active');
+			else if (newMode == 2) removeButton.classList.add('active');
+		}	
+		nodetextButton.addEventListener('click', function () { switchMode(0); }, true);
+		linkButton.addEventListener('click', function () { switchMode(1); }, true);
+		removeButton.addEventListener('click', function () { switchMode(2); }, true);
+	
 		// Initialize Cytoscape
 		cy = cytoscape({
 			container: document.getElementById('cy'),
