12ffd9290b97f82792b00943b233fb137217bfa2
Lionel Laské <llaske@c2s.fr>
2015-08-14 22:09:24 +0200

    Implement modes

--- js/activity.js
+++ js/activity.js
@@ -22,23 +22,72 @@ define(function (require) {
 			else if (newMode == 2) removeButton.classList.add('active');
 		}	
 		nodetextButton.addEventListener('click', function () { switchMode(0); }, true);
-		linkButton.addEventListener('click', function () { switchMode(1); }, true);
+		linkButton.addEventListener('click', function () { switchMode(1); lastSelected = null; }, true);
 		removeButton.addEventListener('click', function () { switchMode(2); }, true);
 		
-		// Initialize Cytoscape
+		// --- Cytoscape handling
+		
+		// Initialize board
 		cy = cytoscape({
 			container: document.getElementById('cy'),
 			
 			ready: function() {
-				// Create first node
-				createNode("<Your idea here>", getCenter());
+				// Create first node and select id
+				var firstNode = createNode(defaultText, getCenter());
+				firstNode.select();
+				lastSelected = firstNode;
+			}
+		});
+		
+		// Event: a node is selected
+		cy.on('select', 'node', function() {
+			if (currentMode == 2) {
+				deleteNode(this);
+				if (lastSelected == this) lastSelected = null;
+				return;
+			} else if (currentMode == 1) {
+				if (lastSelected != null && lastSelected != this) {
+					createEdge(lastSelected, this);
+				}
+				return;
+			}
+			selectNode(this);
+		});
+
+		// Event: a node is unselected
+		cy.on('unselect', 'node', function() {
+			lastSelected = this;
+			unselectNode(this);
+		});
+
+		// Event: an edge is selected
+		cy.on('select', 'edge', function() {
+			if (currentMode == 2) {
+				deleteEdge(this);
+				return;
+			}		
+		});
+		
+		// Event: tap on the board
+		cy.on('tap', function(e){
+			if (e.cyTarget === cy) {
+				if (currentMode == 0) {
+					var newNode = createNode(defaultText, e.cyPosition);
+					if (lastSelected != null)
+						createEdge(lastSelected, newNode);
+					newNode.select();
+				}
 			}
 		});
 
-		// Node handling functions
+		// --- Node and edge handling functions
 		var nodeCount = 0;
 		var edgeCount = 0;
 		var currentFontSize = "16px";
+		var lastSelected = null;
+		var defaultText = "<Your new idea>";
+		
+		// Create a new node with text and position
 		var createNode = function(text, position) {
 			var size = computeStringSize(text, currentFontSize);
 			cy.add({
@@ -65,26 +114,64 @@ define(function (require) {
 				'border-color': 'darkgray',
 				'background-color': 'white',
 				'border-width': '1px',
-				'shape': 'rectangle'
+				'shape': 'roundrectangle'
 			});
-		}
-		var updateNodeText = function(node, text) {
-		}
-		var selectNode = function(node) {
-		}
-		var unselectNode = function(node) {
-		}
-		var deleteNode = function(node) {
+			return newnode;
 		}
 		
-		// Utility functions
+		// Update node text and change size
+		var updateNodeText = function(node, text) {
+			// TODO
+		}
+		
+		// Set node as selected
+		var selectNode = function(node) {
+			node.style({
+				'border-color': 'black',
+				'border-style': 'dashed',
+				'border-width': '4px'
+			});
+		}
+		
+		// Set node as unselected
+		var unselectNode = function(node) {
+			node.style({
+				'border-color': 'darkgray',
+				'border-style': 'solid',					
+				'border-width': '1px'
+			});		
+		}
+		
+		// Delete node, linked edges are removed too
+		var deleteNode = function(node) {
+			cy.remove(node);
+		}
+		
+		// Create a new edge between two nodes
+		var createEdge = function(n1, n2) {
+			cy.add({
+				group: 'nodes',
+				edges: [
+					{ data: { id: 'e'+(++edgeCount), source: n1.id(), target: n2.id() } }
+				]							
+			});		
+		}
+		
+		// Remove an edge
+		var deleteEdge = function(edge) {
+			cy.remove(edge);
+		}
+		
+		// --- Utility functions
+		// HACK: dynamically compute need size putting the string in a hidden div element
 		var computeStringSize = function(text, fontsize) {
-			// HACK: dynamically compute need size putting the string in a hidden div element
 			var computer = document.getElementById("fontsizecomputer");
 			computer.innerHTML = text.replace("<","&lt;").replace(">","&gt;");
 			computer.style.fontSize = fontsize;
 			return {width: (computer.clientWidth+20)+"px", heigth: (computer.clientHeight+4)+"px"};
 		}
+		
+		// Get center of drawing zone
 		var getCenter = function() {
 			var canvas = document.getElementById("canvas");
 			var center = {x: canvas.clientWidth/2, y: canvas.clientHeight/2};
