223e56bd3f4e751be7e1d2759b34714ec3035546
Lionel Laské <llaske@c2s.fr>
2015-08-14 09:52:15 +0200

    Create central node

--- js/activity.js
+++ js/activity.js
@@ -24,15 +24,72 @@ define(function (require) {
 		nodetextButton.addEventListener('click', function () { switchMode(0); }, true);
 		linkButton.addEventListener('click', function () { switchMode(1); }, true);
 		removeButton.addEventListener('click', function () { switchMode(2); }, true);
-	
+		
 		// Initialize Cytoscape
 		cy = cytoscape({
 			container: document.getElementById('cy'),
 			
 			ready: function() {
-				console.log("Hello world !");
+				// Create first node
+				createNode("<Your idea here>", getCenter());
 			}
 		});
+
+		// Node handling functions
+		var nodeCount = 0;
+		var edgeCount = 0;
+		var currentFontSize = "16px";
+		var createNode = function(text, position) {
+			var size = computeStringSize(text, currentFontSize);
+			cy.add({
+				group: 'nodes',
+				nodes: [
+					{
+						data: {
+							id: 'n'+(++nodeCount)
+						},
+						position: {
+							x: position.x,
+							y: position.y
+						}
+					}
+				]							
+			});
+			var newnode = cy.getElementById('n'+nodeCount);
+			newnode.style({
+				'content': text,
+				'width': size.width,
+				'height': size.height,
+				'text-valign': 'center',
+				'text-halign': 'center',
+				'border-color': 'darkgray',
+				'background-color': 'white',
+				'border-width': '1px',
+				'shape': 'rectangle'
+			});
+		}
+		var updateNodeText = function(node, text) {
+		}
+		var selectNode = function(node) {
+		}
+		var unselectNode = function(node) {
+		}
+		var deleteNode = function(node) {
+		}
+		
+		// Utility functions
+		var computeStringSize = function(text, fontsize) {
+			// HACK: dynamically compute need size putting the string in a hidden div element
+			var computer = document.getElementById("fontsizecomputer");
+			computer.innerHTML = text.replace("<","&lt;").replace(">","&gt;");
+			computer.style.fontSize = fontsize;
+			return {width: (computer.clientWidth+20)+"px", heigth: (computer.clientHeight+4)+"px"};
+		}
+		var getCenter = function() {
+			var canvas = document.getElementById("canvas");
+			var center = {x: canvas.clientWidth/2, y: canvas.clientHeight/2};
+			return center;
+		}
     });
 
 });
